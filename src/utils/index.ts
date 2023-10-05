4125773c43b3ee373cfc840402210b9533ea6ddf
Santiago Miranda <santimir@pm.me>
2023-10-05 12:18:39 +0100

    refactor!: new output type, and error if max iterations is reached
    Co-authored-by: Luc Patiny <luc@patiny.com>
    
    The output type now depends in the `info` option object.
    
    When info is true, the return value is:
    
    ```typescript
     {
         resultVector,
         dualVector,
         info: { rse, nIterations }
      }
    ```
    
    Otherwise, the info field won't be present.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,4 @@ export * from './maxWFromZ';
 export * from './checkInputDimensions';
 export * from './getAlpha';
 export * from './updateIndices';
-export * from './getMeanSquaredError';
+export * from './getRootSquaredError';
